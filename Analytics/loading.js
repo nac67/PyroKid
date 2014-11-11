75836cdcfc8e8edbdb263b9e26f0df8d8554ee3b
nac67 <nac67@cornell.edu>
2014-11-11 04:17:56 -0500

    new analytics

--- Analytics/loading.js
+++ Analytics/loading.js
@@ -30,16 +30,64 @@ function load() {
     // ------------------
     var mydata = JSON.parse(data);
 
-    console.log("Number of page loads: " + mydata["page_loads"].length);
+
+    //////// fucking drop off poop fuck shit
+
+    var quests = mydata["player_quests"];
+    var user_quests = {};
+    for (var i = 0; i < quests.length; i++) {
+        var user = quests[i]["user_id"];
+        var quest_array = user_quests[user];
+        if (typeof(quest_array) === "undefined") {
+            user_quests[user] = [];
+        }
+        user_quests[user].push(parseInt(quests[i]["quest_id"]));
+    }
+    for (var user_id in user_quests) {
+        if (user_quests.hasOwnProperty(user_id)) {
+            var quest_array = user_quests[user_id];
+            quest_array = quest_array.sort(function(a,b) { return a - b; });
+            quest_array = quest_array.filter(function(item, pos, self) {
+                return self.indexOf(item) == pos;
+            });
+
+            var i = 0;
+            var first_level_completed = quest_array[0];
+            var highest_consecutive_except_tutorials = 0;
+            for (var j = 0; j < quest_array.length; j++) {
+                if (j > 0 && quest_array[j] > 7 && quest_array[j] != quest_array[j-1] + 1) {
+                    break;
+                }
+                highest_consecutive_except_tutorials = quest_array[j];
+            }
+            if (quest_array.length == 1 && highest_consecutive_except_tutorials > 7) {
+                highest_consecutive_except_tutorials = 0;
+            }
+            for (; i < quest_array.length; i++) {
+                if (quest_array[i] != i + 1) {
+                    break;
+                }
+            }
+            var highest_consecutive = i;
+            var highest_absolute = quest_array[quest_array.length - 1];
+            console.log(user_id + ", " + highest_consecutive + ", " + highest_consecutive_except_tutorials + ", " + highest_absolute);
+        }
+    }
+
+
+    //////// end blurgh pusadflkjalse
+
+
+    //console.log("Number of page loads: " + mydata["page_loads"].length);
 
     var big_ass_hashmap = {}
     mydata["page_loads"].map(function(elem){
         big_ass_hashmap[elem["user_id"]] = true;
     });
-    console.log("Number of unique page loads: " + ParsingUtils.countProperties(big_ass_hashmap));
+    //console.log("Number of unique page loads: " + ParsingUtils.countProperties(big_ass_hashmap));
 
-    console.log("Number of actions: " + mydata["player_actions"].length);
-    console.log("Number of quests: " + mydata["player_quests"].length);
+    //console.log("Number of actions: " + mydata["player_actions"].length);
+    //console.log("Number of quests: " + mydata["player_quests"].length);
 
     var fball_info = {count:0};
     var death_info = {count:0, levels : []};
